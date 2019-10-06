9a90ea0837103ed4a451db57c3001115dae1d5da
Albert221 <w.albert221@gmail.com>
2019-10-06 19:22:08 +0200

    Replace wrong r/pics id on suggested subreddits hardcode

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -63,7 +63,7 @@ func (c *Controller) SuggestedSubredditsHandler(w http.ResponseWriter, r *http.R
 
 	// TODO: Implement collaborative recommending system
 	suggestions := []string{
-		"t5_2sh6t", // pics
+		"t5_2qh0u", // pics
 		"t5_2sbq3", // EarthPorn
 		"t5_2scjs", // CityPorn
 		"t5_2r1tc", // itookapicture
